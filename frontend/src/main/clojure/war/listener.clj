943b6b63f106a3f72ff9a70c54c5a92b037163a6
benjaminsavoy <thefrozenanthem@gmail.com>
2012-08-27 18:24:12 +0200

    cleanup

--- frontend/src/main/clojure/war/listener.clj
+++
@@ -1,8 +0,0 @@
-(ns war.listener
-  (:require [sencha.database :as database])
-  (:gen-class :implements [javax.servlet.ServletContextListener]))
-  
-(defn -contextInitialized [this e]
-  (database/-main))
-  
-(defn -contextDestroyed [this e] )
--- frontend/src/main/clojure/war/servlet.clj
+++
@@ -1,6 +0,0 @@
-(ns war.servlet
-  (:use ring.util.servlet)
-  (:require [sencha.core :as core])
-  (:gen-class :extends javax.servlet.http.HttpServlet))
-
-(defservice core/app)
