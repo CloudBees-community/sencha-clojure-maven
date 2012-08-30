b285f0db1a1ec1bb433bee83925a34ac1183d989
benjaminsavoy <thefrozenanthem@gmail.com>
2012-08-30 17:46:51 +0200

    tests

---
+++ backend/src/test/clojure/sencha/test.clj
@@ -0,0 +1,7 @@
+(ns sencha.test
+  (:use [sencha.core])
+  (:use [sencha.database])
+  (:use [clojure.test]))
+
+(deftest replace-me 
+  (is false "You might want to write some tests for your application, I would. Maybe."))
